9dd2564cb6717b60460769391d19dad4856d45ac
Brian Abel Mamani Condori <bmamanicondori@frba.utn.edu.ar>
2021-05-12 00:03:49 -0300

    F

--- iPortfolio/forms/contact.php
+++ iPortfolio/forms/contact.php
@@ -1,35 +1,16 @@
 <?php
 
-  // Email del destinatario
-  $receiving_email_address = 'bamamanic@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  $destino = "bamamanic@gmail.com"; //creamos variable ($destino) y asignamos el correo del destinatario.
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $name = $_POST["name"];           //almacenamiento de datos ingresados en el formulario
+  $email = $_POST["email"];
+  $subjet = $_POST["subjet"];
+  $message = $_POST["message"];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  $contenido = "Nombre: " . $name . "\nEmail: " . $email . "\nAsunto: " . $subjet . "\nMensaje: " . $message; //Concatenamos las variables.  
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  mail($destino, $subjet, $contenido); //Instruccion para enviar el mail
 
-  echo $contact->send();
+  header("Location:gratitude.html");   //para que nos direccione a otra pagina de agradecimiento
+
 ?>

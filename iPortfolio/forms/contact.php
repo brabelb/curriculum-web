8c12c238cd624ecbe983dee5531f9f169fe347d5
Brian Abel Mamani Condori <bmamanicondori@frba.utn.edu.ar>
2021-05-12 00:55:31 -0300

    01

--- iPortfolio/forms/contact.php
+++ iPortfolio/forms/contact.php
@@ -1,16 +1,12 @@
 <?php
 
   $destino = "bamamanic@gmail.com"; //creamos variable ($destino) y asignamos el correo del destinatario.
-  
   $name = $_POST["name"];           //almacenamiento de datos ingresados en el formulario
   $email = $_POST["email"];
   $subjet = $_POST["subjet"];
   $message = $_POST["message"];
-
   $contenido = "Nombre: " . $name . "\nEmail: " . $email . "\nAsunto: " . $subjet . "\nMensaje: " . $message; //Concatenamos las variables.  
-
   mail($destino, $subjet, $contenido); //Instruccion para enviar el mail
-
-  header("Location:gratitude.html");   //para que nos direccione a otra pagina de agradecimiento
+  //header("Location:gratitude.html");   //para que nos direccione a otra pagina de agradecimiento
 
 ?>
